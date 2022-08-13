fe6e33054f531f6427ef25c8f9a867bce29661b4
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-08-13 18:19:44 +0900

    [init] Project Init

---
+++ idus_Introduction/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  idus_Introduction
+//
+//  Created by 백유정 on 2022/08/13.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

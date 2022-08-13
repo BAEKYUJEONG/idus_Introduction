d3c6f32593576647766e0b58f71772919ae35d5a
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-08-13 19:11:54 +0900

    [feat] API Error 선언

---
+++ idus_Introduction/Network/DetailAPIService.swift
@@ -0,0 +1,23 @@
+//
+//  DetailAPIService.swift
+//  idus_Introduction
+//
+//  Created by 백유정 on 2022/08/13.
+//
+
+import UIKit
+
+enum APIError: Error {
+    case unexpectedStatusCode(statusCode: String)
+    case invalidResponse
+    case noData
+    case failed
+    case invalidData
+}
+
+class SearchAPIService {
+    
+    func getDetail() {
+        
+    }
+}

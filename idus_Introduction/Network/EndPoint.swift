90e9128342683238acfb54cadece18213c908551
BAEKYUJEONG <qordbwjd96@gmail.com>
2022-08-13 20:07:04 +0900

    [feat] Extension 폴더 구성

--- idus_Introduction/Network/EndPoint.swift
+++ idus_Introduction/Network/EndPoint.swift
@@ -19,11 +19,3 @@ extension EndPoint {
         }
     }
 }
-
-extension URL {
-    static let baseURL = "http://itunes.apple.com/lookup"
-    
-    static func makeEndPoint(_ endpoint: String) -> URL {
-        URL(string: baseURL + endpoint)!
-    }
-}
